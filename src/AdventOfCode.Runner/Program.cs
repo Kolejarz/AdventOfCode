43a9fe0eacc6a928f5c83fbc2c7b8c51c8626d38
Kolejarz <contact@tomaszkras.net>
2024-12-03 08:15:18 +0100

    Day3 part 2

--- src/AdventOfCode.Runner/Program.cs
+++ src/AdventOfCode.Runner/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Year2015.Solvers;
 
-var solver = new Year2024.Solvers.Day02();
-var solution = solver.PartTwo(solver.PuzzleInput.Value);
+var solver = new Year2024.Solvers.Day03();
+var solution = solver.PartOne(solver.PuzzleInput.Value);
 
 Console.WriteLine(solution);
---
+++ src/Year2024/Solvers/Day03.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode.Commons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Year2024.Solvers;
+
+public class Day03 : Solver<List<(int, int)>, int>
+{
+    public Day03() : base(2024, 3)
+    {
+    }
+
+    public override List<(int, int)> ParseInput(IEnumerable<string> input)
+    {
+        return Regex.Matches(string.Join(string.Empty, input), @"mul\((\d+),(\d+)\)")
+            .Select(m => m.Groups)
+            .Select(x => (int.Parse(x[1].Value), int.Parse(x[2].Value))).ToList();
+    }
+
+    public override int PartOne(List<(int, int)> input)
+    {
+        return input.Select(x => x.Item1 * x.Item2).Sum();
+    }
+
+    public override int PartTwo(List<(int, int)> input)
+    {
+        throw new NotImplementedException();
+    }
+}
